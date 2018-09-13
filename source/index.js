2920175a1219a64a904d20b281375a42b7e09533
Levabala <levabala@ya.ru>
2018-09-13 17:26:14 +0300

    1.0.1

--- source/index.js
+++ source/index.js
@@ -1,3 +1,12 @@
 import YAML from 'yamljs';
 
-module.exports = YAML.load('./.eslintrc.yml');
+const config = YAML.load('./.eslintrc.yml');
+console.log(config);
+
+const base_config = {
+  rules: {
+    'no-console': 2,
+  },
+};
+
+module.exports = base_config;
